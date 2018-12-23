971e9cd07e23d94f1a858cdd2df9d3808928e003
fengyuhan <fengyuhan_1@hotmail.com>
2018-12-23 05:25:09 -0600

    Update MaterialService.cs
    convert some pesudo code into semi pesudo code.

--- Product.Services/MaterialService.cs
+++ Product.Services/MaterialService.cs
@@ -29,14 +29,46 @@ namespace Product.Services
         {
             // find list of product:
             // 1 get all the product where has meterial by id 2
+            IList<MaterialModel> localCopyMaterialList = GetAll();
+            if(localCopyMaterialList == null)
+            {
+                //  TODO: print null 
+                return;
+            }
+            foreach(var m in localCopyMaterialList)
+            {
+                if(m!= null)
+                {
+                    // see if m.id == materialIdToDelete
+                    // if match add to a local list of Material
+                }
+            }
             //
-            // 2 for each product replace 2 with 1
+            // 2 for each product in the list replace 2 with 1
+            //  start for loop
             // replacement:
             //      if  already using product 1 then quantity ++
-            //      else set productMaterial.MaterialName(1.name) set productMaterial.MaterialID (1.id)
-            // 3 remove:
+            //          and then remove 2 in the product ingreadent list (remove 2 from list of material)
+            //      else set productMaterial.MaterialName(1.name) 
+            //           set productMaterial.MaterialID (1.id) 
+            // 3 remove in DB:
             // in material table rm by id 2
+            // end for loop
+
+            // temp list list of product
+            // somehow get list of Product
+            // for each Product get ICollection<ProductMaterial> ProductMaterials
+            //      for each ProductMaterials for each where MaterialId == materialIdToDelete
+            //           add this.Product to temp list
+            //  end of for loop
+            // for each product in temp list
+            //      replacement(Product p, Int32 materialIdToKeep, Int32 materialIdToDelete)
+            // end of for loop
             throw new NotImplementedException("TODO: Please implement me");
+        }
+        private void replacement(Product product, Int32 materialIdToKeep, Int32 materialIdToDelete)
+        {
+            product.setName();
         }
 
         private MaterialModel Transfer(Data.Entities.Material entity)
